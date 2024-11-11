c35ca400e6d0ca9b3ca96296c4cd7eef5b397278
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2024-11-10 19:35:25 -0700

    extending the person class into subclass patient !

--- core/patient.py
+++ core/patient.py
@@ -1,7 +1,17 @@
-class Patient:
+from person import *
+
+
+class Patient(Person):
     def __init__(
-        self, medical_history: list[dict] = None, insurance_details: list[dict] = None
+        self,
+        name: str,
+        age: int,
+        gender: Literal["Male", "Female"],
+        contact_info: ContactInfo,
+        medical_history: list[dict] = None,
+        insurance_details: list[dict] = None,
     ) -> None:
+        super().__init__(name, age, gender, contact_info)
         # Initialize medical history, defaulting to an empty list if none provided
         self.medical_history = medical_history if medical_history is not None else []
 
