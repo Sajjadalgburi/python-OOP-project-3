817b24094aeb769304018eb319f15414431fb667
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2024-11-10 19:24:29 -0700

    initial commit for appointment class

---
+++ appointments/appointment.py
@@ -0,0 +1,12 @@
+class Appointment:
+    def __init__(self) -> None:
+        pass
+
+    def schedule(self):
+        pass
+
+    def re_schedule(self):
+        pass
+
+    def cancle_schedule(self):
+        pass
