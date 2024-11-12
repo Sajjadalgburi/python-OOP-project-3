fb4e186aae97fa4bf707a568694ae80a454b6f31
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2024-11-11 18:37:33 -0700

    created custom method to access every registred person within tje hospital

--- core/hospital.py
+++ core/hospital.py
@@ -1,10 +1,12 @@
+from typing import Literal
+
 # Acts as a central hub, managing core functionalities and linking classes.
 class Hospital:
     def __init__(
         self,
         name: str,
         location: str,
-        departments: list,
+        departments: list[str],
     ) -> None:
         self.name = name
         self.location = location
@@ -13,15 +15,14 @@ class Hospital:
             'Surgery',
             'Cardiology', # specializes in heart and cardiovascular health
             'Pediatrics', # provides care for children and adolescents.
-          
         ] 
-        self.__staff_list = []
+        self.__nurse_list = []
         self.__patient_list = []
-        self.__doctors = []
+        self.__doctor_list = []
         self.appointments = [] # stores all appointments made
         
 
-        # Add a new department to the hospital.
+    # Add a new department to the hospital.
     def add_department(self):
         pass
 
@@ -35,7 +36,7 @@ class Hospital:
     def view_schedule(self):
         pass
 
-        # Generate reports for hospital administration.
+    # Generate reports for hospital administration.
     def generate_reports(self):
         pass
     
@@ -46,3 +47,20 @@ class Hospital:
     # list of the future appointments
     def future_appointments(self):
         pass
+    
+    def access_catalog(self, input: Literal["Doctors", "Nurses", "Patients"] = "Doctors"):
+        valid_inputs = ["Doctors", "Nurses", "Patients"]
+        
+        if input not in valid_inputs:
+            raise TypeError('input must be "Doctors", "Nurses", or "Patients"')
+        
+        # Mapping input to corresponding list
+        catalog = {
+            "Doctors": self.__doctor_list,
+            "Nurses": self.__nurse_list,
+            "Patients": self.__patient_list
+        }
+        
+        print(f"\nAll of the {input.lower()} in the hospital system: ")
+        for person in catalog[input]:
+            print(person)
